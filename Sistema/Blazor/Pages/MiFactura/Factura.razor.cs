abda21e7bf4f06ba0c32435fb3c242454ce47263
CLucas792 <carloslucas792@gmail.com>
2023-04-25 18:05:34 -0600

    Aporte Inicial

---
+++ Sistema/Blazor/Pages/MiFactura/Factura.razor.cs
@@ -0,0 +1,23 @@
+﻿using Blazor.Interfaces;
+using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Modelos;
+
+namespace Blazor.Pages.MiFactura
+{
+    public partial class Factura
+    {
+        [Inject] private IEstablecimientoServicio establecimientoServicio { get; set; }
+        [Inject] private NavigationManager navigationManager { get; set; }
+        [Inject] private SweetAlertService Swal { get; set; }
+
+        private Establecimiento establecimiento = new Establecimiento();
+
+        protected override async Task OnInitializedAsync()
+        {
+
+            establecimiento = await establecimientoServicio.UnicoRegistroAsync();
+
+        }
+    }
+}
