87789da789e3de3307d38f0bec422c51cd06e4eb
CLucas792 <carloslucas792@gmail.com>
2023-04-30 15:15:52 -0600

    Quinto Commit

--- Sistema/Blazor/Pages/MiLaboratorio/ReciboExamenes.razor.cs
+++ Sistema/Blazor/Pages/MiLaboratorio/ReciboExamenes.razor.cs
@@ -1,10 +1,1 @@
-﻿using Modelos;
-
-namespace Blazor.Pages.MiLaboratorio
-{
-    public partial class ReciboExamenes
-    {
-        private Examen examen = new Examen();
-        private IEnumerable<Examen> lista { get; set; }
-    }
-}
+﻿
